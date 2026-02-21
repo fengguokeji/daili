1122340c4d19900dad5a596ec5a6d2a10d2c59fb
shadowrocket <fenguokeji@gmail.com>
2026-02-21 12:45:33 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Short Link Service</title>
+        <title>短链接测试</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </Head>
